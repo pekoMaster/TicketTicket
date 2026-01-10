1b696030fca1564958bfa796a1c8d6ac14cc3ee1
pekomaster <pekopekopekopekomura@gmail.com>
2026-01-11 01:41:32 +0800

    v1.08 - Update version numbers

--- src/components/layout/SideNav.tsx
+++ src/components/layout/SideNav.tsx
@@ -103,7 +103,7 @@ export default function SideNav() {
           </a>
         </div>
         <p className="text-xs text-gray-400 dark:text-gray-500 text-center">
-          TicketTicket v1.07
+          TicketTicket v1.08
         </p>
       </div>
     </aside>
